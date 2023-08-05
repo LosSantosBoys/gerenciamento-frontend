176e6ec6cc45a75d43dc9b96db13b281b1c2e67e
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-04 21:12:35 -0300

    adiciona postData

--- src/app/service/api-service.ts
+++ src/app/service/api-service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { FuncionarioRequest } from "../models/funcionario/funcionario-model";
 
 @Injectable()
 export class ApiService {
-    // TODO: Adicionar URL da API
-    private url = 'https://jsonplaceholder.typicode.com/posts/1';
+    private url = 'http://localhost:8080/api/v1/';
 
     constructor(private http: HttpClient) { }
 
     getData(): Observable<any> {
         return this.http.get<any>(this.url);
+    }
+
+    postData(body: FuncionarioRequest): Observable<any> {
+        return this.http.post<any>(this.url + 'usuario/funcionario', body);
     }
 }
