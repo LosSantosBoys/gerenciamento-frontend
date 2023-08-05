5b786b10924c3373f94a02ae2256b80a0a3f0e0b
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-04 21:08:00 -0300

    Create funcionario-model.ts

---
+++ src/app/models/funcionario/funcionario-model.ts
@@ -0,0 +1,9 @@
+export interface FuncionarioRequest {
+    nome: string;
+    cpf: string;
+    email: string;
+    telefone: string;
+    cargo: string;
+    username: string;
+    senha: string;
+}
