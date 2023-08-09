53cc448e80f7823b2b5ffe1cb3a1b0cc461a296c
Samuel Marques <samuel.s.marques@protonmail.com>
2023-08-09 17:51:20 -0300

    Update register.component.ts

--- src/app/pages/user/auth/register/register.component.ts
+++ src/app/pages/user/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FuncionarioRequest } from 'src/app/models/funcionario/funcionario-model';
+import { ApiService } from 'src/app/service/api-service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css', '../../../../styles/authentication.css']
 })
 export class RegisterComponent {
+  funcionario: FuncionarioRequest = {
+    nome: '',
+    cpf: '',
+    email: '',
+    username: '',
+    senha: '',
+    cargo: '',
+    telefone: '',
+  };
 
+  constructor(private apiService: ApiService) { }
+
+  onSubmit(): void {
+    this.apiService.postData(this.funcionario).subscribe({
+      error: (error) => console.error(error),
+      complete: () => console.info('cadastrado'),
+    });
+  }
 }
